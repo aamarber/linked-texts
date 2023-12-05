bdc7799e6033e053e442f33c2ad92c4f07cbf20c
melocotoneroh <aamarber@gmail.com>
2023-12-05 19:31:12 +0100

    Possible solution

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
+  const [userName, setUsername] = useState('')
+
+  return (
+    <>
+      <h1>
+        Hello {userName}!
+      </h1>
+      <div>
+        <label htmlFor="userName">Username:</label>
+        <input type="text" id="userName" value={userName} onChange={e => setUsername(e.target.value) } />
+      </div>
+    </>
+  )
 }
 
 export default App
