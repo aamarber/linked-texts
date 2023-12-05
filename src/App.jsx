77d92ac1c03e58376f023798edc4a70db694ccb3
melocotoneroh <aamarber@gmail.com>
2023-12-05 19:46:19 +0100

    Colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [userName, setUsername] = useState('')
+  const [userName, setUsername] = useState('xxx');
+  const [color, setColor] = useState('white');
 
   return (
     <>
-      <h1>
+      <h1 style={{color: color}}>
         Hello {userName}!
       </h1>
       <div>
         <label htmlFor="userName">Username:</label>
-        <input type="text" id="userName" value={userName} onChange={e => setUsername(e.target.value) } />
+        <input type="text" id="userName" value={userName} onChange={e => setUsername(e.target.value) } 
+        style={{color: color}}/>
+      </div>
+      <div>
+        <label htmlFor="color">Color:</label>
+        <input type="text" id="color" value={color} onChange={e => setColor(e.target.value) } />
       </div>
     </>
   )
